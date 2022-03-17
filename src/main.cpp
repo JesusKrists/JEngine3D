fa6b016263bd3d1aece5ac6d8193a24e88c2c378
JesusKrists <krists.n99@gmail.com>
2022-03-17 19:57:16 +0100

    Fix build error

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <spdlog/spdlog.h>
 
 // This file will be generated automatically when you run the CMake configuration step.
-// It creates a namespace called `myproject`.
+// It creates a namespace called `JEngine3D`.
 // You can modify the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
@@ -34,8 +34,8 @@ int main(int argc, const char **argv)
       { std::next(argv), std::next(argv, argc) },
       true,// show help if requested
       fmt::format("{} {}",
-        myproject::cmake::project_name,
-        myproject::cmake::project_version));// version string, acquired from config.hpp via CMake
+        JEngine3D::cmake::project_name,
+        JEngine3D::cmake::project_version));// version string, acquired from config.hpp via CMake
 
     for (auto const &arg : args) { std::cout << arg.first << "=" << arg.second << '\n'; }
 
