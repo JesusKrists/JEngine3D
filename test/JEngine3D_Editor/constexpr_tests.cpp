35a0c0c9288d6eb6518b029e6d0453df02c9f13c
JesusKrists <krists.n99@gmail.com>
2022-03-24 11:42:39 +0000

    Refactor tests to separate JEngine3D and JEngine3D_Editor

---
+++ test/JEngine3D_Editor/constexpr_tests.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch_test_macros.hpp>
+
+constexpr auto Factorial(unsigned int number) -> unsigned int// NOLINT(misc-no-recursion)
+{
+  return number <= 1 ? number : Factorial(number - 1) * number;
+}
+
+TEST_CASE("Factorials are computed with constexpr", "[factorial]")
+{
+  STATIC_REQUIRE(Factorial(1) == 1);
+  STATIC_REQUIRE(Factorial(2) == 2);
+  STATIC_REQUIRE(Factorial(3) == 6);
+  STATIC_REQUIRE(Factorial(10) == 3628800);
+}
